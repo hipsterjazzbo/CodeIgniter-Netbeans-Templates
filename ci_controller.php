3c15aaa05d59747ced842b0347318998044390de
Caleb Fidecaro <calebfidecaro@gmail.com>
2011-10-05 09:30:52 +1300

    First Commit

---
+++ ci_controller.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class ${name?capitalize} extends CI_Controller {
+    
+	function __construct() 
+	{
+		parent::__construct();
+	}
+}
+
+/* End of file ${nameAndExt} */
+/* Location: ./application/controllers/${nameAndExt} */
